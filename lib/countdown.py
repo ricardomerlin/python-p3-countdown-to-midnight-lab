ef4a95efe6b6e479828dc68b41c97e6a6e0ed735
Ricardo Merlin <rcrdmerlin@gmail.com>
2023-12-14 02:16:21 -0500

    uploading working lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,14 @@
 # your code goes here!
+import time
+
+def countdown (sec):
+    while sec > 0:
+        print(f'{sec} SECOND(S)!')
+        countdown_with_sleep(sec)
+        sec = sec - 1
+    print('HAPPY NEW YEAR!')
+    
+
+def countdown_with_sleep(sec):
+    time.sleep(1)
+
